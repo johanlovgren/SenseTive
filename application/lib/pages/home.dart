c5ecefe270795756cf2dce888e912a06cd02f40b
Johan.Lovgren <johan.lovgren.5795@student.uu.se>
2021-04-05 11:49:08 +0200

    Added base app structure

--- application/lib/pages/home.dart
+++ application/lib/pages/home.dart
@@ -6,14 +6,64 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  Widget _currentPage;
+  int _currentIndex;
+  List _listPages = [];
+
+
+  @override
+  void initState() {
+    super.initState();
+    // Todo Add pages in _listPages and set current page
+    // _listPages..add(Page1())..add(Page2());
+    // _currentPage = Page1();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: Text('Home'),
+      appBar: AppBar(
+        title: Text('Home'),
+      ),
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Container(),
+          // TODO Change this
+          //child: _currentPage,
         ),
-        body: Container(),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: 0,
+        // TODO Change this
+        //currentIndex: _currentIndex,
+        items: [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Home'),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.history),
+            label: 'History'
+          ),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.account_circle),
+              label: 'User page'
+          ),
+        ],
+        onTap: (selectedIndex) => _changePage(selectedIndex),
+      ),
     );
+  }
+
+  void _changePage(int selectedIndex) {
+
+    /*
+    // TODO Fix this
+    setState(() {
+      _currentIndex = selectedIndex;
+      _currentPage = _listPages[selectedIndex];
+    });
+     */
   }
 }
 
