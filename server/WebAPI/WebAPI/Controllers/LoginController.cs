a187d429d4959d6a59c52daf7497a9ffdce15e06
Simon Pettersson <rickebo@rickebo.com>
2021-04-06 16:03:03 +0200

    Added initial server files

---
+++ server/WebAPI/WebAPI/Controllers/LoginController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class LoginController : ControllerBase
+    {
+        private readonly ILogger<LoginController> _logger;
+
+        public LoginController(ILogger<LoginController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public LoginResponse Get() => new LoginResponse(ok: false)
+        {
+            
+        };
+
+        public class LoginResponse
+        {
+            public bool Ok { get; } 
+
+            public LoginResponse(bool ok)
+            {
+                Ok = ok;
+            }
+        }
+    }
+}
