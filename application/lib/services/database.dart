63ee23af7738ffdf39c4ded6a4d1556737d1cbfb
Johan.Lovgren <johan.lovgren.5795@student.uu.se>
2021-04-14 16:48:43 +0200

    Added documentation in Database.dart

--- application/lib/services/database.dart
+++ application/lib/services/database.dart
@@ -3,18 +3,21 @@ import 'dart:io';
 import 'dart:convert';
 import 'package:sensetive/models/reading_models.dart';
 
+/// File routines used for persistent storage
 class DatabaseFileRoutines {
+  /// Get the local path to persistent storage
   Future <String> get _localPath async {
     final directory = await getApplicationDocumentsDirectory();
     return directory.path;
   }
-
+  /// Get the local file containing the stored data
   Future<File> get _localFile async {
     final path = await _localPath;
 
     return File('$path/local_persistence.json');
   }
 
+  /// Get stored readings ([Reading]) as JSON
   Future<String> readReadings() async {
     try {
       final file = await _localFile;
@@ -31,22 +34,26 @@ class DatabaseFileRoutines {
     }
   }
 
+  /// Write readings ([Reading])to persistent storage
   Future<File> writeReadings(String json) async{
     final file = await _localFile;
     return file.writeAsString('$json');
   }
 }
 
+/// Create a [Database] from a JSON
 Database databaseFromJson(jsonString) {
   final dataFromJson = json.decode(jsonString);
   return Database.fromJson(dataFromJson);
 }
 
+/// Create a JSON from a [Database]
 String databaseToJson(Database data) {
   final dataToJson = data.toJson();
   return json.encode(dataToJson);
 }
 
+/// Database containing data to be stored in persistent storage
 class Database {
   List<Reading> readings;
   Database({
