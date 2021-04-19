4a573794e39d283cee3e315a0e826076842b901e
Simon Pettersson <rickebo@rickebo.com>
2021-04-19 15:19:32 +0200

    Added documentation

--- server/WebAPI/ServerLib/AccountManager.cs
+++ server/WebAPI/ServerLib/AccountManager.cs
@@ -20,6 +20,12 @@ namespace ServerLib
             _database = database;
         }
 
+        /// <summary>
+        /// Retrieves a user with the specified authentication details if one exists, otherwise, one is created.
+        /// </summary>
+        /// <param name="method">The authentication method used by the user</param>
+        /// <param name="identifier">The authentication identifier used by the user</param>
+        /// <returns>The user associated with the authentication details</returns>
         public async Task<DbUser> GetOrCreateUser(AuthenticationMethod method, string identifier)
         {
             await using var userTable = await _database.Table<DbUser>()
@@ -58,8 +64,16 @@ namespace ServerLib
             return user;
         }
 
+        /// <summary>
+        /// Creates a user in the database and returns the created user
+        /// </summary>
+        /// <typeparam name="TConnection">The type of connection used to communicate with the database</typeparam>
+        /// <param name="userTable">The table to insert the user into</param>
+        /// <param name="configurator">A function that can be used to modify the user</param>
+        /// <returns>The created user</returns>
         public async Task<DbUser> CreateUser<TConnection>(
-            TableConnectionContext<DbUser, TConnection> userTable)
+            TableConnectionContext<DbUser, TConnection> userTable,
+            Action<DbUser> configurator = null)
             where TConnection : DbConnection
         {
             var userId = Guid.NewGuid();
@@ -70,6 +84,8 @@ namespace ServerLib
                 Authorization = AuthorizationLevel.User.ToString()
             };
 
+            configurator?.Invoke(user);
+
             var inserted = await userTable.Insert(user).ConfigureAwait(false);
 
             if (inserted < 1)
@@ -78,8 +94,19 @@ namespace ServerLib
             return user;
         }
 
+        /// <summary>
+        /// Creates a user login in the database and returns the created user login
+        /// </summary>
+        /// <typeparam name="TConnection">The type of connection used to communicate with the database</typeparam>
+        /// <param name="loginTable">The database table to add the user login to</param>
+        /// <param name="user">The database user to link the user login to</param>
+        /// <param name="authMethod">The authentication method used for the login</param>
+        /// <param name="authIdentifier">The authentication identifier used for the login</param>
+        /// <param name="configurator">A function that can be used to modify the user login</param>
+        /// <returns>The created user login</returns>
         public async Task<DbUserLogin> CreateLogin<TConnection>(
-            TableConnectionContext<DbUserLogin, TConnection> loginTable, DbUser user, AuthenticationMethod authMethod, string authIdentifier)
+            TableConnectionContext<DbUserLogin, TConnection> loginTable, DbUser user, AuthenticationMethod authMethod, string authIdentifier,
+            Action<DbUserLogin> configurator = null)
             where TConnection : DbConnection
         {
             var authMethodString = authMethod.ToString();
@@ -89,12 +116,16 @@ namespace ServerLib
                 query => query.Where(nameof(DbUserLogin.LoginMethod), "=", authMethodString),
                 query => query.Where(nameof(DbUserLogin.LoginIdentifier), "=", authIdentifier));
 
-            await loginTable.Insert(new DbUserLogin()
-                {
-                    UserId = user.UserId,
-                    LoginMethod = authMethodString,
-                    LoginIdentifier = authIdentifier
-                })
+            var userLogin = new DbUserLogin()
+            {
+                UserId = user.UserId,
+                LoginMethod = authMethodString,
+                LoginIdentifier = authIdentifier
+            };
+
+            configurator?.Invoke(userLogin);
+
+            await loginTable.Insert(userLogin)
                 .ConfigureAwait(false);
 
             return await loginTable.SelectFirstOrDefault(queryProcessor)
--- server/WebAPI/ServerLib/Data/DbUser.cs
+++ server/WebAPI/ServerLib/Data/DbUser.cs
@@ -18,6 +18,9 @@ namespace ServerLib.Data
         [DatabaseColumn("VARCHAR(36)", IsPrimaryKey = true, NotNull = true)]
         public string UserId { get; set; }
 
+        /// <summary>
+        /// The authorization level of the user
+        /// </summary>
         [DatabaseColumn("VARCHAR(16)", NotNull = true, Default = "\'User\'")]
         public string Authorization { get; set; }
 
@@ -26,6 +29,10 @@ namespace ServerLib.Data
 
         }
 
+        /// <summary>
+        /// Retrieves the authorization level of the user in enum form
+        /// </summary>
+        /// <returns>The authorization level of the user in enum form</returns>
         public AuthorizationLevel GetAuthorizationLevel() =>
             Enum.TryParse(typeof(AuthorizationLevel), Authorization, out var authLevel)
                 ? (AuthorizationLevel) authLevel
