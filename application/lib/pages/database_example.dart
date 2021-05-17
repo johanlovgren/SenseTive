576ec5fc666e4297801bd4fa01002f9a9aa58901
Johan.Lovgren <johan.lovgren.5795@student.uu.se>
2021-05-17 09:29:45 +0200

    Added example code

--- application/lib/pages/database_example.dart
+++ application/lib/pages/database_example.dart
@@ -43,16 +43,33 @@ class _DatabaseExampleState extends State<DatabaseExample> {
   }
 
   Reading dummyReading() {
+    int duration = 60 + Random().nextInt(60*60-200);
     return Reading(
       // Fix better solution for ID's
-      id: Random().nextInt(2^32),
-      date: DateTime.now(),
-      durationSeconds: 60 + Random().nextInt(60*60-200),
-      momHeartRate: [60,62,64,61,57,60,60,62,64,61,57,60,60,62,64,61,57,60,60,62,64,61,57,60],
-      babyHeartRate: [60,62,64,61,57,60,60,62,64,61,57,60,60,62,64,61,57,60,60,62,64,61,57,60],
-      oxygenLevel: null,
-      contractions: null
+        id: Random().nextInt(2^32),
+        date: DateTime.now().subtract(Duration(days: Random().nextInt(30))),
+        durationSeconds: duration,
+        momHeartRate: _momRandomHeartRate(duration),
+        babyHeartRate: _babyRandomHeartRate(duration),
+        oxygenLevel: null,
+        contractions: null
     );
+  }
+
+  List<int> _momRandomHeartRate(int duration) {
+    List<int> heartRate = [];
+    for (int i = 0; i < duration / 60; i++) {
+      heartRate.add(55 + (Random().nextBool() ? 1 : -1) * Random().nextInt(2));
+    }
+    return heartRate;
+  }
+
+  List<int> _babyRandomHeartRate(int duration) {
+    List<int> heartRate = [];
+    for (int i = 0; i < duration / 60; i++) {
+      heartRate.add(140 + (Random().nextBool() ? 1 : -1) * Random().nextInt(2));
+    }
+    return heartRate;
   }
 }
 
